b9462b4bd08f844bbd8efc8141cf6a83989e2a13
Sylvan Smit <sjsmit@amazon.com>
2023-07-27 19:18:07 +0200

    parsing the json now

--- get_kernel_versions.rs
+++ get_kernel_versions.rs
@@ -2,11 +2,51 @@
 
 //! ```cargo
 //! [dependencies]
-//! reqwest = { version = "0.11", features = ["blocking"] }
+//! reqwest = { version = "0.11", features = ["blocking", "json"] }
+//! serde = { version = "1", features = ["derive"] }
 //! ```
 
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Stable {
+    version: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Released {
+    timestamp: u64,
+    isodate: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Patch {
+    full: Option<String>,
+    incremental: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Release {
+    iseol: bool,
+    version: String,
+    moniker: String,
+    source: Option<String>,
+    pgp: Option<String>,
+    released: Released,
+    gitweb: String,
+    changelog: Option<String>,
+    diffview: Option<String>,
+    patch: Patch,
+}
+
+#[derive(Debug, Deserialize)]
+struct Response {
+    latest_stable: Stable,
+    releases: Vec<Release>,
+}
 
 fn main() {
     let response = reqwest::blocking::get("https://www.kernel.org/releases.json").unwrap();
-    println!("{}", response.text().unwrap());
+    let var: Response = response.json().unwrap();
+    println!("{:?}", var);
 }
